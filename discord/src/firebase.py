26a711c5cb12aff13b9c5ab54098dd27ad599cd7
adam jacobs <adamjacobs272@gmail.com>
2024-04-08 13:20:39 -0400

    firebase api

--- discord/src/firebase.py
+++ discord/src/firebase.py
@@ -1,7 +1,8 @@
 import discord
 import json
-from firebase_admin import db
-
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 """
   schools {
     clubs {
@@ -17,11 +18,28 @@ from firebase_admin import db
 
 """
 
+
+def Connect_To_Firestore():
+  cred = credentials.Certificate('./firestore-config.json')
+  app = firebase_admin.initialize_app(cred)
+  db = firestore.client()
+  return db
+  
 async def Validate_Poster(school, club, id):
   ref = db.reference("/" + school + "/" + club + "/posters")
   return id in ref
 
-async def Generate(content):
-  ref = db.reference("/posts")
-  ref.set(content)
+def Post(db, school, club, announcment, id):
+  # Assuming `id` contains the ID of the document you want to set data to
+  parent_doc_ref = db.collection("ClubData").document("school_id").collection("clubs").document("club_id")
+
+  # Define the data you want to set to the document as a dictionary
+  data = {
+    "posters": [], # Empty dictionary for posters subcollection
+    "links": [],   # Empty dictionary for links subcollection
+    "announcements": [] # Empty dictionary for announcements subcollection
+  }
+
+  # Set the data to the document
+  parent_doc_ref.update(data)
 
