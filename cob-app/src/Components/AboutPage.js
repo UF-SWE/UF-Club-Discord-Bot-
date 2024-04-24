3383979b2e795c13e3c1a74382eaf0ec7a94183d
FloorMatt89 <matthewsegura89@gmail.com>
2024-04-24 16:02:40 -0400

    HomePage updated and other front end changes

--- cob-app/src/Components/AboutPage.js
+++ cob-app/src/Components/AboutPage.js
@@ -3,8 +3,17 @@ import React from 'react';
 
 function AboutPage() {
   return (
-    <div>
+    <div className ="about-container">
       <h2>About Us</h2>
+      <div className="text-box"> 
+        <p>The goal for C.O.B. is to connect clubs here at the University of Florida to one single hub. 
+          By serving as a central platform, C.O.B. facilitates seamless communication and collaboration among various student organizations, 
+          fostering a vibrant and interconnected campus community. With C.O.B., clubs can easily share resources, coordinate events, and collaborate
+          on initiatives, ultimately enhancing the overall student experience and enriching campus life. Whether it's organizing club fairs, promoting
+          leadership development opportunities, or facilitating cross-club projects, C.O.B. is dedicated to empowering student leaders and strengthening
+          the bonds between clubs, ensuring that every student finds their place and thrives within our diverse and dynamic university ecosystem.
+        </p>
+      </div>
     </div>
   );
 }
